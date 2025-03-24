fb41daaf92a4f57511230a5fb0dd498792a230a6
ArielM24 <ariel_montoya15@outlook.es>
2025-03-24 12:41:04 -0600

    check spawn status

---
+++ src/main/java/com/sg/illusioner/IEntityDataSaver.java
@@ -0,0 +1,7 @@
+package com.sg.illusioner;
+
+import net.minecraft.nbt.NbtCompound;
+
+public interface IEntityDataSaver {
+    NbtCompound getPersistentData();
+}
--- src/main/java/com/sg/illusioner/SGIllusioner.java
+++ src/main/java/com/sg/illusioner/SGIllusioner.java
@@ -6,6 +6,9 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.EvokerEntity;
 import net.minecraft.entity.mob.IllusionerEntity;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.Difficulty;
 
 import java.util.Random;
@@ -18,7 +21,7 @@ public class SGIllusioner implements ModInitializer {
 
 	public static final Random r = new Random();
 
-	public static final int spawnRatio = 50;
+	public static final int spawnRatio = 85;
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -31,13 +34,35 @@ public class SGIllusioner implements ModInitializer {
 			if (!entity.getType().equals(EntityType.EVOKER)) {
 				return;
 			}
+			EvokerEntity evoker = (EvokerEntity) entity;
+			NbtElement check = ((IEntityDataSaver)evoker).getPersistentData().get("illusioner_spawn");
+			if(check != null){
+				return;
+			}
+			((IEntityDataSaver)evoker).getPersistentData().putString("illusioner_spawn", "checked");
 			boolean willSpawn = r.nextInt(100) <= spawnRatio;
 			if (!willSpawn) {
 				return;
 			}
-			EvokerEntity evoker = (EvokerEntity) entity;
+			int xOffset = 1 + r.nextInt(2);
+			int zOffset = 1 + r.nextInt(2);
+			xOffset = r.nextBoolean() ? xOffset : -xOffset;
+			zOffset = r.nextBoolean() ? zOffset : -zOffset;
+			BlockPos pos = evoker.getBlockPos();
+			BlockPos spawnPos = new BlockPos(pos.getX() + xOffset, pos.getY(), pos.getZ() + zOffset);
+			BlockPos floor = spawnPos.offset(Direction.DOWN);
+			BlockPos air = spawnPos.offset(Direction.UP, 1);
+			if(!evoker.getWorld().getBlockState(floor).isOpaqueFullCube()){
+				return;
+			};
+			if(!evoker.getWorld().getBlockState(spawnPos).isAir()){
+				return;
+			};
+			if(!evoker.getWorld().getBlockState(air).isAir()){
+				return;
+			};
 			IllusionerEntity illusioner = EntityType.ILLUSIONER.create(serverLevel, SpawnReason.NATURAL);
-			illusioner.setPos(evoker.getPos().getX(), evoker.getPos().getY(), evoker.getPos().getZ());
+			illusioner.setPosition(spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5);
 			serverLevel.spawnNewEntityAndPassengers(illusioner);
 		});
 	}
